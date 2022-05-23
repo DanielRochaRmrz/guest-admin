944e4b77f61a2ba140d3c863e92e363825bcd524
marpez4 <87680723+marpez4@users.noreply.github.com>
2022-05-23 15:06:15 -0500

    Update monitoreo

--- src/pages/admin-monitear-reserv/admin-monitear-reserv.ts
+++ src/pages/admin-monitear-reserv/admin-monitear-reserv.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MonitoreoReservasProvider } from '../../providers/monitoreo-reservas/monitoreo-reservas';
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
-import { AdminDeMonUserPage } from '../admin-de-mon-user/admin-de-mon-user';
 import { AdminLeeQrPage } from '../admin-lee-qr/admin-lee-qr';
 import { AdminReservacionDetallePage } from '../admin-reservacion-detalle/admin-reservacion-detalle';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -21,7 +20,6 @@ export class AdminMonitearReservPage {
   scannedData: any = {};
 
   reservaciones: any = [];
-  clientes: any;
   datosQr: any;
   menu: any;
   formatoFecha: any;
@@ -42,94 +40,114 @@ export class AdminMonitearReservPage {
   }
 
   ionViewDidLoad() {
+
     this.getAllReservacione();
-    this.getClientes();
+
     this.getEvento();
+
   }
 
   getAllReservacione() {
+
     var dateObj = new Date()
+
     var anio = dateObj.getFullYear().toString();
+
     var mes = dateObj.getMonth().toString();
+
     var dia = dateObj.getDate();
+
     var mesArray = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+
     if (dia >= 1 && dia <= 9) {
+
       var diaCero = '0' + dia;
+
       this.formatoFecha = anio + '-' + mesArray[mes] + '-' + diaCero;
+
     } else {
+
       this.formatoFecha = anio + '-' + mesArray[mes] + '-' + dia;
+
     }
-    console.log("fecha actual");
-    console.log(this.formatoFecha);
+
+    console.log("fecha actual", this.formatoFecha);
+    
     const id = localStorage.getItem('uidSucursal');
+    
     //Cuando es un usuario se saca el id de la sucursal ala que pertenece
     this.afs.collection('users', ref => ref.where('uid', '==', id)).valueChanges().subscribe(data => {
+
       this.sucursales = data;
+
       this.sucursales.forEach(element => {
+
         const uidSucursal = element.uidSucursal;
+
         this.monRes.getReservaciones(uidSucursal, this.formatoFecha).subscribe(reser => {
+
           this.reservaciones = reser;
+
           console.log('reservaciones1', reser);
+          
         })
+        
+        // TRAE LAS RESERVACIONES COMPARTIDAS
         this.monRes.getReservacionesAcepCom(uidSucursal, this.formatoFecha).subscribe(reser => {
+
           this.reservacionesAcep = reser;
+
           console.log('reservaciones2', reser);
+
         })
       });
     });
-    this.monRes.getReservaciones(id, this.formatoFecha).subscribe(reser => {
-      this.reservaciones = reser;
-      console.log('reservaciones3', reser);
-    })
-    this.monRes.getReservacionesAcepCom(id, this.formatoFecha).subscribe(reser => {
-      this.reservacionesAcep = reser;
-      console.log('reservaciones4', reser);
-    })
+
   }
-  getClientes() {
-    this.monRes.getAllClientes("users").then(c => {
-      this.clientes = c;
-      console.log("Estos son los clientes: ", this.clientes);
-    });
-  }
+
   goDetalle(idReservacion) {
+
     this.navCtrl.push(AdminReservacionDetallePage, { idReservacion: idReservacion });
+
   }
-  //aceptarReserv(idReservacion) {
-  //this.navCtrl.push(AdminLeeQrPage, {idReservacion:idReservacion});
-  //}
 
   getEvento() {
+
     this.afs.collection("evento").valueChanges().subscribe(data12 => {
         this.infoEvento = data12;
         console.log("evento", this.infoEvento);
     });
+
   }
 
   scan() {
+
     this.options = {
+
       prompt: 'Procesando Reservación'
+
     };
+
     this.barcodeScanner.scan(this.options).then((data) => {
+
       this.scannedData = data;
+
       this.datosQr = data.text;
-      //console.log('this.datosQr');
-      //console.log(this.datosQr);
-      //obtener los datos del QR del cliente y sacarlo de uno por uno en la variable que manda el scanner
-      //const dataCode = JSON.parse(this.datosQr);
-      //console.log('dataCode: ', dataCode.idReservacion);
-      //console.log('dataCode: ', dataCode.total);
-      //console.log('dataCode: ', dataCode.idUsuario);
-      //console.log('dataCode: ', dataCode.tarjeta);
+
       const datosQr = this.datosQr;
+
       this.navCtrl.push(AdminLeeQrPage, { datosQr: datosQr });
-      //alert('La reservación cambio de estatus');
+
     }, (err) => {
+      
       console.log('Error', err);
     })
   }
+
   behind(){
+
     this.navCtrl.setRoot(AdminMenuReservacionPage);
+
   }
 
 }
